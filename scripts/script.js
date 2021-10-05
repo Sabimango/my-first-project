23764d05ded1c0de21f11c5bfb6907cbbe5b5533
Sabimango <yuifrim@gmail.com>
2021-10-05 20:35:36 +0200

    First Commit
    Main page, collection page, images, styles & script. More to add.

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+//image changing on the frontpage
+document.addEventListener("DOMContentLoaded", () => {
+    const log = console.log,
+      array = ["./images/img1.jpg", "./images/img4.jpg", "./images/img5.jpg"],
+      random = Math.floor(Math.random() * array.length),
+      target = document.getElementById("target");
+    target.src = `${array[random]}`;
+    log(target);
+  });
+  
