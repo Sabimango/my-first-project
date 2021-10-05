21b8e9d49f6fb43a2bd2f30769de830b92bf40f1
Sabimango <yuifrim@gmail.com>
2021-10-05 20:56:08 +0200

    Second Commit
    Added images, changed script, linked pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 //image changing on the frontpage
 document.addEventListener("DOMContentLoaded", () => {
     const log = console.log,
-      array = ["./images/img1.jpg", "./images/img4.jpg", "./images/img5.jpg"],
+      array = ["./images/img1.jpg", "./images/img2.jpg"],
       random = Math.floor(Math.random() * array.length),
       target = document.getElementById("target");
     target.src = `${array[random]}`;
