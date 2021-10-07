e815fc5b92f8da2b4b755ffc8f1bae33db0b73df
Sabimango <yuifrim@gmail.com>
2021-10-07 23:25:16 +0200

    Contact
    Added contact HTML + CSS, changed minor issues on other pages, connected the HTMLs

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
 //image changing on the frontpage
 document.addEventListener("DOMContentLoaded", () => {
-    const log = console.log,
-      array = ["./images/img1.jpg", "./images/img2.jpg"],
-      random = Math.floor(Math.random() * array.length),
-      target = document.getElementById("target");
-    target.src = `${array[random]}`;
-    log(target);
-  });
-  
+  const log = console.log,
+    array = ["./images/img1.jpg", "./images/img2.jpg"],
+    random = Math.floor(Math.random() * array.length),
+    target = document.getElementById("target");
+  target.src = `${array[random]}`;
+  log(target);
+});
